d9eb03ff37b560309cf0f9bde265bd9555383d10
Grace <graceeilori@gmail.com>
2026-02-15 13:02:28 -0700

    button edit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.png" />
             </head>
             <body className={`${dmSans.variable} ${ibmPlexSans.variable} font-body antialiased`}>
                 {children}
